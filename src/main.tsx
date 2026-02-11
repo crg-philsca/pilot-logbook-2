03367a1507fbf3effb84901020344ee988066856
crgphilsca <238011821+crg-philsca@users.noreply.github.com>
2026-02-11 20:05:19 +0800

    Force Service Worker registration

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,19 @@ if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/sw.js').then(registration => {
       console.log('SW registered: ', registration);
+
+      // If there's an active worker, we're good
+      if (registration.active) return;
+
+      // If there's a new worker, wait for it to be ready, then reload
+      registration.addEventListener('updatefound', () => {
+        const newWorker = registration.installing;
+        newWorker?.addEventListener('statechange', () => {
+          if (newWorker.state === 'activated') {
+            window.location.reload();
+          }
+        });
+      });
     }).catch(registrationError => {
       console.log('SW registration failed: ', registrationError);
     });
